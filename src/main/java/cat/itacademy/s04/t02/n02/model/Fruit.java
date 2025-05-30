f8ed59219bb65272577468a93e204784458df429
Sergio Serrano <sergiomelgara@gmail.com>
2025-05-30 08:59:46 +0200

    Initial commit

---
+++ src/main/java/cat/itacademy/s04/t02/n02/model/Fruit.java
@@ -0,0 +1,39 @@
+package cat.itacademy.s04.t02.n02.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Fruit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double quilsQuantity;
+
+    public Fruit(){
+    }
+
+    public Fruit(String name, double quilsQuantity) {
+        this.name = name;
+        this.quilsQuantity = quilsQuantity;
+    }
+
+    public double getQuilsQuantity() {
+        return quilsQuantity;
+    }
+
+    public void setQuilsQuantity(double quilsQuantity) {
+        this.quilsQuantity = quilsQuantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/cat/itacademy/s04/t02/n02/repository/FruitRepository.java
@@ -0,0 +1,7 @@
+package cat.itacademy.s04.t02.n02.repository;
+
+import cat.itacademy.s04.t02.n02.model.Fruit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FruitRepository extends JpaRepository<Fruit, Integer> {
+}
